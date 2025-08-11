c5ee18abf657f3df344b9b4c1c2902dff12721cc
rookedsysc <rookedsysc36@gmail.com>
2025-08-11 23:00:30 +0900

    feat: f2 정의로 이동으로 설정

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- 정의로 이동
+map("n", "<F2>", vim.lsp.buf.definition, { desc = "Go to Definition" })
+
