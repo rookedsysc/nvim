b9546b4eeb82229a7fc34712cfe46a603c15339a
rookedsysc <rookedsysc36@gmail.com>
2025-08-24 23:50:57 +0900

    feat: encoding 설정 추가

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,12 @@
 
 -- Clipboard 안씀 -> XClip 사용
 vim.opt.clipboard = "unnamedplus"
+
+-- UTF-8 인코딩 설정
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = "utf-8"
+
 -- 절대 라인 번호 표시
 vim.opt.number = true
 vim.opt.relativenumber = false -- 상대 라인 번호 비활성화
