f17b7828b0938fea73098359751d7bee3c98288f
rookedsysc <rookedsysc36@gmail.com>
2025-08-24 23:46:45 +0900

    feat: Oil 단축키

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,5 @@ local map = vim.keymap.set
 -- 정의로 이동
 map("n", "<F2>", vim.lsp.buf.definition, { desc = "Go to Definition" })
 
+-- Oil
+vim.keymap.set("n", "oi", "<CMD>Oil<CR>", { desc = "Open Oil with file's current directory" })
