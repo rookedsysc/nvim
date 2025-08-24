9426d0d07ce51d9848fe2fcd1f006c2a1651d1bf
rookedsysc <rookedsysc36@gmail.com>
2025-08-24 23:46:37 +0900

    delete: 버퍼 삭제 단축키

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,3 @@ local map = vim.keymap.set
 -- 정의로 이동
 map("n", "<F2>", vim.lsp.buf.definition, { desc = "Go to Definition" })
 
--- 버퍼 삭제
-map("n", "<S-w>", "<cmd>bdelete<CR>", { desc = "Delete Buffer" })
