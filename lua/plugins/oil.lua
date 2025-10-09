1de95492943b306c953ac3af3e0b714dec604714
rookedsysc <rookedsysc36@gmail.com>
2025-10-09 14:02:24 +0900

    feat: 숨김 파일 표시

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,12 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      -- .env 같은 숨김 파일 표시
+      show_hidden = true,
+    },
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
