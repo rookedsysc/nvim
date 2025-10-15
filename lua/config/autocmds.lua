500ebd789cd51a5d1c6fd1117443139a25ebbc14
rookedsysc <rookedsysc36@gmail.com>
2025-10-15 20:43:05 +0900

    feat: 특정 파일 타입만 autoformat 활성화

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- 특정 파일타입만 autoformat 활성화
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "java", "typescript", "kotlin", "yaml", "json" },
+  callback = function()
+    vim.b.autoformat = true
+  end,
+})
